e758ce40044a34c3a27398c150dda34725295668
doobaryreza@gmail.com <doobaryreza@gmail.com>
2020-08-18 17:14:23 +0100

    Gaussian Classification implemented and code cleanup elsewhere

--- GaussianProcesses.py
+++ GaussianProcesses.py
@@ -1,214 +1,15 @@
+#######################################################
+#
+# Python script responsible for implemented the GPs, 
+# particularly, the regression as well as the 
+# classification cases.
+#
+#######################################################
+
 import numpy as np
 import pandas as pd
 from scipy.linalg import inv
-
 from numpy.linalg import cholesky, det, lstsq
-from scipy.optimize import minimize
-
-import matplotlib.pyplot as plt
-
-import warnings
-
-class kernel:
-    def __init__(self):
-        pass
-    
-    def covariance(self, X1, X2):
-        pass
-    
-    def log_prob(self, X, Y, noise):
-        pass
-    
-    def log_prob_core(self, X, Y, noise, naive_implementation = False):
-        if not naive_implementation:
-            K = self.covariance(X,X) + noise * np.eye(len(X))
-
-            L = cholesky(K)
-
-            return np.sum(np.log(np.diagonal(L))) + \
-                   0.5 * Y.T.dot(lstsq(L.T, lstsq(L, Y)[0])[0]) + \
-                   0.5 * len(X) * np.log(2*np.pi)
-        else:
-            K = self.covariance(X,X) + noise * np.eye(len(X))
-            #print(det(K))
-            return 0.5 * np.log(det(K)) + \
-                   0.5 * Y.T.dot(inv(K).dot(Y)) + \
-                   0.5 * len(X) * np.log(2*np.pi)
-    
-    def fit(self, X, Y, noise):
-        pass
-    
-    def reset_parameters(self):
-        pass
-    
-    
-class SquareExponential(kernel):
-    def __init__(self, length_scale = 1.0, deviation = 1.0):
-        self.length_scale = length_scale
-        self.deviation = deviation
-        self.printed = False
-        
-    def covariance(self, X1, X2):     
-        diff = (X1[:,np.newaxis] - X2)
-        square_distance_matrix = np.sum(diff**2,2)
-        
-        return self.deviation**2 * np.exp(-0.5 / self.length_scale**2 * square_distance_matrix)
-    
-    def log_prob(self, X, Y, noise, naive_implementation = False, length_scale = None, deviation = None):
-        if isinstance(length_scale, float):
-            self.length_scale = length_scale
-        if isinstance(deviation, float):
-            self.deviation = deviation
-        
-        return self.log_prob_core(X,Y,noise, naive_implementation)
-    
-    def fit(self, X, Y, noise=1e-10, init = [1,1], bounds = ((1e-5, None), (1e-5, None)), method = 'L-BFGS-B'):
-
-        def to_min(parameter):
-            try:
-                return self.log_prob(X,Y,noise,naive_implementation = False,\
-                                     length_scale = parameter[0],deviation = parameter[1])
-            except Exception as e:
-                if not self.printed:
-                    print('Using unstable log_prob due to the following issue:',e)
-                    self.printed = True
-                else:
-                    pass
-                return self.log_prob(X,Y,noise,naive_implementation = True,\
-                                     length_scale = parameter[0],deviation = parameter[1])
-        res = minimize(to_min, init, bounds=bounds,method=method)
-        
-    def reset_parameters(self, length_scale, deviation):
-        self.length_scale = length_scale
-        self.deviation = deviation
-        
-    def __str__(self):
-        outstring = ''
-        outstring+='Type : \n'
-        outstring+='\t Square Exponential \n'
-        outstring+='Parameters : \n'
-        outstring+='\t length_scale : '+str(self.length_scale)+'\n'
-        outstring+='\t deviation : '+str(self.deviation)
-        return outstring
-    
-    
-class RationalQuadraticKernel(kernel):
-    def __init__(self, length_scale = 1.0, deviation = 1.0, relative_scaling = 1.0):
-        self.length_scale = length_scale
-        self.deviation = deviation
-        self.relative_scaling = relative_scaling
-        self.printed = False
-        
-    def covariance(self, X1, X2):
-        diff = (X1[:,np.newaxis] - X2)
-        square_distance_matrix = np.sum(diff**2,2)
-        square_distance_matrix = 1 + square_distance_matrix/(2*self.relative_scaling*self.length_scale*self.length_scale)
-        return self.deviation**2 * square_distance_matrix**(-self.relative_scaling)
-    
-    
-
-    
-    def log_prob(self, X, Y, noise,naive_implementation = False, length_scale = None, deviation = None, relative_scaling = None):
-        if isinstance(length_scale, float):
-            self.length_scale = length_scale
-        if isinstance(deviation, float):
-            self.deviation = deviation
-        if isinstance(relative_scaling, float):
-            self.relative_scaling = relative_scaling
-            
-        return self.log_prob_core(X,Y,noise, naive_implementation)
-        
-    def fit(self, X, Y, noise=1e-10, init = [1,1,1], bounds = ((1e-5, None), (1e-5, None),(1e-5, None)), method = 'L-BFGS-B'):
-        def to_min(parameter):
-            try:
-                return self.log_prob(X,Y,noise,naive_implementation = False,length_scale = parameter[0],deviation = parameter[1], \
-                                 relative_scaling = parameter[2])
-            except Exception as e:
-                if not self.printed:
-                    print('Using unstable log_prob due to the following issue:',e)
-                    self.printed = True
-                else:
-                    pass
-                return self.log_prob(X,Y,noise,naive_implementation = True,length_scale = parameter[0],deviation = parameter[1], \
-                                 relative_scaling = parameter[2])
-        res = minimize(to_min, init, bounds=bounds,method=method)
-        
-    def reset_parameters(self, length_scale, deviation, relative_scaling):
-        self.length_scale = length_scale
-        self.deviation = deviation
-        self.relative_scaling = relative_scaling
-        
-    def __str__(self):
-        outstring = ''
-        outstring+='Type : \n'
-        outstring+='\t Rational Quadratic Kernel \n'
-        outstring+='Parameters : \n'
-        outstring+='\t length_scale : '+str(self.length_scale)+'\n'
-        outstring+='\t deviation : '+str(self.deviation)+'\n'
-        outstring+='\t relative_scaling : '+str(self.relative_scaling)
-        return outstring
-        
-class PeriodicKernel(kernel):
-    """
-    Wierd error coming from positive definite covariance....already seen in:
-    https://stackoverflow.com/questions/55103221/cholesky-decomposition-positive-semidefinite-matrix
-    """
-    def __init__(self, length_scale = 1, deviation =1, period = 1):
-        self.length_scale = length_scale
-        self.deviation = deviation
-        self.period = period
-        self.printed = False
-        
-    def covariance(self, X1, X2):
-        diff = (X1[:,np.newaxis] - X2)
-        square_distance_matrix = np.sum(diff**2,2)
-        
-        sin_arg = (np.pi*np.sqrt(square_distance_matrix))/self.period
-        sin_part = np.sin(sin_arg)**2/(self.length_scale**2)
-        
-        return self.deviation**2 * np.exp(-2*sin_part)
-        
-    
-    def log_prob(self, X, Y, noise, naive_implementation = False, length_scale = None, deviation = None, period = None):
-        if isinstance(length_scale, float):
-            self.length_scale = length_scale
-        if isinstance(deviation, float):
-            self.deviation = deviation
-        if isinstance(period, float):
-            self.period = period
-            
-        return self.log_prob_core(X,Y,noise, naive_implementation)
-    
-    def fit(self, X, Y, noise = 1e-10, init = [1,1,1], bounds = ((1e-5, None), (1e-5, None), (1e-5,None))\
-            , method = 'L-BFGS-B'):
-        def to_min(parameter):
-            try:
-                return self.log_prob(X,Y,noise,naive_implementation = False,length_scale = parameter[0],deviation = parameter[1], \
-                                 period = parameter[2])
-            except Exception as e:
-                if not self.printed:
-                    print('Using unstable log_prob due to the following issue:',e)
-                    self.printed = True
-                else:
-                    pass
-                return self.log_prob(X,Y,noise,naive_implementation = True,length_scale = parameter[0],deviation = parameter[1], \
-                                 period = parameter[2])
-        res = minimize(to_min, init, bounds=bounds,method=method)
-    
-    def reset_parameters(self, length_scale, deviation, relative_scaling):
-        self.length_scale = length_scale
-        self.deviation = deviation
-        self.period = period
-        
-    def __str__(self):
-        outstring = ''
-        outstring+='Type : \n'
-        outstring+='\t Period Kernel \n'
-        outstring+='Parameters : \n'
-        outstring+='\t length_scale : '+str(self.length_scale)+'\n'
-        outstring+='\t deviation : '+str(self.deviation)+'\n'
-        outstring+='\t period : '+str(self.period)
-        return outstring
         
 class GuassianProcessRegression:
     def __init__(self, kernel, noise = 1e-10):
@@ -216,8 +17,16 @@ class GuassianProcessRegression:
         self.noise = noise
         
     def _nan_checker(self, covariance_matrix):
+        """
+        A null checker function that is required when taking the square root of the diagonal
+        components of the variance matrix. This is required for numercal stability reason, as
+        it is possible that we have a very small negative result which we force to be zero.
+
+        TO DO: this needs to be removed.
+        """
+
         diag_part = np.diag(covariance_matrix)
-        diag_part_copy = diag_part.copy()
+        diag_part_copy = diag_part.copy() ### very very very tiny negative numbers which are essentially zero.
         diag_part_copy[diag_part < 0] = 0
         diag_part = diag_part_copy
         std = np.sqrt(diag_part)
@@ -226,7 +35,6 @@ class GuassianProcessRegression:
     def _posterior(self, new_point, return_cov = False, return_std = False):
         K_XX = self.kernel.covariance(self.X,self.X) + self.noise*np.eye(len(self.X))
         K_sX = self.kernel.covariance(new_point, self.X)
-        print(K_sX)
         K_ss = self.kernel.covariance(new_point, new_point)
         K_XX_inv = inv(K_XX)
 
@@ -244,6 +52,10 @@ class GuassianProcessRegression:
         
         
     def fit(self, X, y, config = None):
+        """
+        Fitting function for the GP regression - this simply fits the hyperparameters.
+        Possible config object used if we want to use a slightly different optimisation methodology.
+        """
         self.X = X.copy()
         self.y = y.copy()
         mu_y = self.y.mean()
@@ -264,3 +76,89 @@ class configs:
         self.init = init
         self.bounds = bounds
         self.method = method
+
+### sigmoid for logisitic regression - should be moved to a different file at some point.
+class Sigmoid:
+    def __init__(self):
+        pass
+    
+    def prob(self, z):
+        return 1/(1+np.exp(-z))
+    
+    def first_derivative_condtional(self, class_, f_):
+        # class_ distributition, conditioned on f_
+        return (class_ + 1)/2 - self.prob(f_)
+    
+    def second_dervative_conditional(self, class_, f_):
+        # class_ distributition, conditioned on f_
+        return -self.prob(f_)*(1- self.prob(f_))
+
+
+class GaussianClassification:
+    def __init__(self, kernel, class_prob):
+        self.kernel = kernel
+        self.class_prob = class_prob
+        
+    def _laplace_approximation(self, f_init, max_iter, tol):
+        """
+        This implements the laplace approximation exactly as Rasmussen and Williams - Gaussian Processes for Machine Learning book.
+        The laplace approximation needs to applied to: P(y|X,c)~ P(gaussian_latent_output|  data, classification).
+        """
+        prev_logq = None
+        f = f_init
+
+        for i in range(0,max_iter):
+            
+            W = np.diag(-self.class_prob.second_dervative_conditional(self.y, f))
+            B = np.eye(len(self.K_XX)) + np.matmul(np.matmul(np.sqrt(W), self.K_XX), np.sqrt(W))
+            L = cholesky(B)
+            b = np.matmul(W, f) + self.class_prob.first_derivative_condtional(self.y, f)
+
+            B_inv = inv(B)
+            a = b - np.matmul(np.matmul(np.matmul(np.matmul(np.sqrt(W),B_inv), np.sqrt(W)),self.K_XX),b)
+            f = np.matmul(self.K_XX, a)
+
+            logq = -np.matmul(a,f) + np.sum(-np.log(1+np.exp(-self.y*f))) - np.sum(np.log(np.diag(L)))
+            if prev_logq is None:
+                prev_logq = logq
+            else:
+                if abs(prev_logq - logq) < tol:
+                    break
+            prev_logq = logq
+            
+        return f, logq
+
+    
+    def fit(self, X, y, max_iter = 1000, tol = 10e-6):
+        """
+        The fitting function simply applies the laplace approximation to P(y|X,c)~ P(gaussian_latent_output|  data, classification)
+        """
+        self.X = X.copy()
+        self.y = y.copy()
+        
+        self.K_XX = self.kernel.covariance(X, X)
+        f = np.array([0]*len(X))
+        f, logq = self._laplace_approximation(f, max_iter, tol)
+            
+        self.logq = logq
+        self.f = f
+    
+    def predict(self, new_data): 
+
+        W = np.diag(self.class_prob.prob(self.f)*(1- self.class_prob.prob(self.f)))
+        B = np.eye(len(self.f)) + np.matmul(np.matmul(np.sqrt(W), self.K_XX), np.sqrt(W))
+        L = cholesky(B)
+
+        K_sX = self.kernel.covariance(new_data, self.X)
+        
+        K_ss  = np.diag(np.diag(self.kernel.covariance(new_data,new_data)))
+        
+        v = np.matmul(K_sX,np.matmul(inv(L),np.sqrt(W)))
+        
+        
+        var = np.diag(np.diag(K_ss - np.matmul(v,v.T)))
+        y_mean = np.matmul(K_sX, self.class_prob.first_derivative_condtional(self.y, self.f))
+        return y_mean, var
+    
+    def classify(self, y_mean, threshold = 0.5):
+        return np.array(self.class_prob.prob(y_mean) > threshold,int)
